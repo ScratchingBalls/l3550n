f246b2f86ec8b2f0c74fadd258897c64dabc297b
ScratchingBalls <endrik-allen.saagim@vikk.ee>
2020-11-30 15:20:58 +0200

    testing with wikipedia stuff

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const appDiv = document.getElementById("app"); //select element with id 'app'
+//appDiv.innerHTML = `<h1>Ur mums a hoe</h1>`;    //inject something into that element
+
+function reqListener() {
+  appDiv.innerHTML = JSON.parse(this.responseText).extract_html; //inject something into that element
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open(
+  "GET",
+  "https://en.wikipedia.org/api/rest_v1/page/summary/Command_%26_Conquer"
+);
+oReq.send();
